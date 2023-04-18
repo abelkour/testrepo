7c93862b4a98fd7b004d4a1ed80e21f9b1441173
Amine Belkoura <mxb135330@utdallas.edu>
2023-04-17 19:43:13 -0500

    initial version

---
+++ singleprocess.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+                 printf("This is statement 1. \n");
+                 printf("This is statement 2. \n");
+		 sleep(20);
+                 return(0);
+}
+
