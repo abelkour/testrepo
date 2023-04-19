40c3e39cc5c1df46f0258e5a3b1fec3cc7ef3496
Amine Belkoura <mxb135330@utdallas.edu>
2023-04-19 12:12:22 -0500

    remove one printout

--- singleprocess.cpp
+++ singleprocess.cpp
@@ -5,7 +5,6 @@ int main(void)
 {
                  // A second comment
 		 printf("This is statement 1. \n");
-                 printf("This is statement 2. \n");
 		 sleep(20);
                  return(0);
 }
